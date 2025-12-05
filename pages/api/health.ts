8ff1145acb30781b157aecbfb19d6c5889f6f136
joshuahenry3108 <joshua.sahayam@rootquotient.com>
2025-12-05 13:11:03 +0530

    Change deployment name from 'A' to 'B'

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -9,7 +9,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     // 👇 change this before each deployment
-    const deploymentName = "Deployment-A";  
+    const deploymentName = "Deployment-B";  
     const timestamp = new Date().toISOString();
 
     return res.status(200).json({
